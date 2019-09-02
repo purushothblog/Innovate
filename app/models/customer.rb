88de720b8dcb05edb172616dc386df55b162e353
PURUSHOTHAMAN <pruzoth@gmail.com>
2019-09-03 00:02:03 +0530

    Working sample

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,21 @@
 class Customer < ApplicationRecord
+ def marker
+ 	if self.atm.present?
+ 		"Atm Present"
+ 		if self.mobile.present?
+ 			self.mark = 10
+ 			else
+ 			self.mark = 5
+ 		end
+ 	elsif
+ 		"Atm Absent"
+ 		if self.mobile.present?
+ 			self.mark = 5
+ 		else
+ 			self.mark = 0
+ 		end
 
-	def marker
-		@customer = Customer.update(mark:[params])
+ 	end
+ 	self.save!
 	end
 end
